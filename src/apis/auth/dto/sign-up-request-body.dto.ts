01fbcbe3f1fa8782420b531c122d41e9948e9242
LeeSeokHo <rrgks@naver.com>
2023-10-18 18:37:33 +0900

    [#9]/feat: request validator 추가

--- src/apis/auth/dto/sign-up-request-body.dto.ts
+++ src/apis/auth/dto/sign-up-request-body.dto.ts
@@ -1,7 +1,13 @@
+import { IsEmail, IsString, Length } from 'class-validator';
+
 export class SignUpRequestBodyDto {
+  @Length(3, 10)
+  @IsString()
   name: string;
 
+  @IsEmail()
   email: string;
 
+  @IsString()
   password: string;
 }
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,27 @@
+import {
+  BadRequestException,
+  ValidationError,
+  ValidationPipe,
+} from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+      stopAtFirstError: true,
+      whitelist: true,
+      exceptionFactory: (errors: ValidationError[]) => {
+        const messages = Object.values(errors[0].constraints!)[0];
+
+        throw new BadRequestException(messages);
+      },
+    }),
+  );
+
   await app.listen(3000);
 }
 bootstrap();
